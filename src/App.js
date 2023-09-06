73ffdbb56d0ed155ec9e4465d150ae4a65be754e
Johnny Tai <bm414148@gmail.com>
2023-09-06 13:38:19 +0800

    import PriceInput

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import { PriceInput } from "./components/priceinput/PriceInput.tsx";
 
 function App() {
-  return <div className="">ASIA YO</div>;
+  return (
+    <div className="">
+      ASIA YO
+      {/* <PriceInput /> */}
+    </div>
+  );
 }
 
 export default App;
