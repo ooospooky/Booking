6fba3e80afce57c94bcb2d47da3acf1f2ee6229c
Johnny Tai <bm414148@gmail.com>
2023-09-07 13:32:51 +0800

    add remove element btn

--- src/components/ageGroupPriceList/AgeGroupPriceList.tsx
+++ src/components/ageGroupPriceList/AgeGroupPriceList.tsx
@@ -10,24 +10,50 @@ export interface AgeGroupData {
 }
 
 export const AgeGroupPriceList = () => {
-  const [data, setData] = useState<AgeGroupData[]>([{ ageGroup: [0, 20], price: '0' }, { ageGroup: [0, 20], price: '0' }, { ageGroup: [0, 20], price: '0' }])
+  const [data, setData] = useState<AgeGroupData[]>([{ ageGroup: [0, 20], price: '0' }])
 
   const [isOverLap, setIsOverLap] = useState(false);
+  const [notInclude, setNotInclude] = useState(false);
 
   //when data change check if allAgeGroup overlap
   //if overlap's array > 0 mean is overlap, set overlap to ture, else false
   useEffect(() => {
+    console.log('result:', data)
     const allAgeGroups = data.map(item => item.ageGroup);
     const result = getNumberIntervals(allAgeGroups);
+    console.log('get', result);
     result.overlap.length > 0 ? setIsOverLap(true) : setIsOverLap(false);
+    result.notInclude.length > 0 ? setNotInclude(false) : setNotInclude(true);
   }, [data])
+
+  const handleAddCard = () => {
+    setData((prev) => {
+      const newData = [...prev];
+      newData.push({ ageGroup: [0, 20], price: '0' });
+      return newData
+    })
+  }
+  const handleCardRemove = (index: number) => {
+    setData((prev) => {
+      const newData = [...prev];
+      newData.splice(index, 1)
+      return newData
+    })
+  }
 
   return (
     <div className="ageGroupPriceList">
       {data.map((item, index) => {
         return (
           <div key={index} className="ageGroupPriceList__container">
-            <h1 className="ageGroupPriceList__container--title">價格設定 - {index + 1}</h1>
+            <div className="ageGroupPriceList__titleDiv">
+              <h1 className="ageGroupPriceList__titleDiv--title">價格設定 - {index + 1}</h1>
+              {index !== 0 &&
+                <button
+                  onClick={() => handleCardRemove(index)}
+                  className="ageGroupPriceList__titleDiv--remove">X 移除</button>
+              }
+            </div>
             <div className="ageGroupPriceList__card">
               <AgeGroupSelect
                 setData={setData}
@@ -39,11 +65,14 @@ export const AgeGroupPriceList = () => {
                 data={data}
                 index={index} />
             </div>
+            {index !== data.length - 1 && <hr />}
           </div>
         )
       })}
-      AgeGroupPriceList
-
+      <button
+        disabled={notInclude}
+        className={`ageGroupPriceList__addCard ${notInclude && 'ageGroupPriceList__addCard--disable'}`}
+        onClick={handleAddCard}>+ 新增價格設定</button>
     </div>
 
   )
