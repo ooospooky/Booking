1a516268dd2e08f0f2b1593c7f612fdc24301722
Johnny Tai <bm414148@gmail.com>
2023-09-08 01:34:11 +0800

    add reduxProvider to enable redux

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import "./index.css";
 import App from "./App.tsx";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { ReduxProvider } from "./redux/features/provider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ReduxProvider>
+      <App />
+    </ReduxProvider>
   </React.StrictMode>
 );
 
